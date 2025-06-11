26637b93d4855ab4df58d238312ecbde0a0f8609
fernando <oliveira-fernando1@hotmail.com>
2025-06-11 18:54:07 -0300

    update

--- batch/silver/src/main/scala/Main.scala
+++ batch/silver/src/main/scala/Main.scala
@@ -2,7 +2,6 @@ import config.SparkConfig
 import service.{DeltaService, KafkaService}
 
 object Main {
-
   def main(args: Array[String]): Unit = {
     val spark = SparkConfig.createSession()
     try {
@@ -13,5 +12,4 @@ object Main {
       spark.stop()
     }
   }
-
 }
