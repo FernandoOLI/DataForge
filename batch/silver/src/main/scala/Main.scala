0ecf467f84a0175f9911d491068c3b8aa16b4355
fernando <oliveira-fernando1@hotmail.com>
2025-06-11 18:50:08 -0300

    add ci-cd silver

--- batch/silver/src/main/scala/Main.scala
+++ batch/silver/src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import config.SparkConfig
 import service.{DeltaService, KafkaService}
 
 object Main {
+
   def main(args: Array[String]): Unit = {
     val spark = SparkConfig.createSession()
     try {
@@ -12,4 +13,5 @@ object Main {
       spark.stop()
     }
   }
+
 }
